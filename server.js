f647d22a16a6641e74dc99f670da308107229ee9
aaaziz2 <aumio.aziz@gmail.com>
2022-08-25 23:14:17 -0500

    got api/notes post to work, added app.get to render notes page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express()
 const notes = require('./db/db.json')
 
@@ -15,6 +16,10 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
 app.get('/api/notes', (req, res) => {
     res.status(200).json(notes);
 })
@@ -23,27 +28,39 @@ app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to add a note`)
   
-    const response = {
-    status: 'success',
+    const newNotes = {
+    title: req.body.title,
     id: uuid(),
-    body: req.body,
+    text: req.body.text,
     };
       
-    console.log(response);
-    notes.push(response)
+    readAndAppend(newNotes, './db/db.json')
+
+    const response = {
+        status: 'success',
+        body: req.body,
+      };
+     
+    console.log(req.body)
+      
     res.status(201).json(response);
 })
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+// extra credit will work on that later
 app.delete('/api/notes/:id', (req,res) => {
 
     console.info(`${req.method} request received to delete a note`)
 
-    let keys = Object.keys(notes)
-    for(let i = 0; i<keys.length; i++){
-        if(keys[i] == id){
-            index = notes.indexOf(i)
-            notes.splice(index,1)
-        }
+    // let keys = Object.keys(notes)
+    // for(let i = 0; i<keys.length; i++){
+    //     if(keys[i] == id){
+    //         index = notes.indexOf(i)
+    //         notes.splice(index,1)
+    //     }
 
-    }
+    // }
 })
